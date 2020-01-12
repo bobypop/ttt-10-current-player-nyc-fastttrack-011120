2aba0c76d2c435bfc3d25dbc2621e51a14db0d38
alex chee <alexchee95@gmail.com>
2020-01-12 18:43:08 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  board.each do |x|
+  counter = 0
+  if x == "O" || x == "X"
+  counter += 1
+else
+  counter += 0
+  end
+counter
+end
+end
+
+turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
+
+
+#board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
+  
