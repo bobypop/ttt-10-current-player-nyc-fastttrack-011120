c7fda3f3cb4c6a389a89f17290b53e00dda6918d
alex chee <alexchee95@gmail.com>
2020-01-12 18:51:31 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+def turn_count(board)
+  counter = 0
+  board.each do |x|
+  if x == "O" || x == "X"
+  counter += 1
+  else
+  counter += 0
+  end
+end
+ counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+  "X"
+else "O"
+end
+end
+
+puts turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
+
+
+#board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
