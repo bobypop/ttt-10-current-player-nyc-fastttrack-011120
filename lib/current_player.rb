23446becbeeed29905c3fd7cf266a393e32d4613
alex chee <alexchee95@gmail.com>
2020-01-12 18:51:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,10 +10,8 @@ end
  counter
 end
 
-counter = turn_count(board)
-
-def current_player(counter)
-  if counter % 2 == 0
+def current_player(board)
+  if turn_count(board) % 2 == 0
   "X"
 else "O"
 end
